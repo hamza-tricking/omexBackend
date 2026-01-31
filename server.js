1020118a4201d1a213ae191bf6ca3bc74d0f69a2
Mohamed <hamzatricks@gmail.com>
2026-01-31 22:12:00 +0100

    authinaction4

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: ['http://localhost:3001', 'https://dmtart.pro'],
     credentials: true
 }));
 app.use(express.json());
